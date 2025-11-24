05f4ef551e2d87a559128bc2a2d36ece7fe72dd8
houcine1amraoui <houcine1amraoui@gmail.com>
2025-11-24 10:32:18 +0100

    updated

--- main.py
+++ main.py
@@ -4,16 +4,6 @@ from bst import BST
 # BST instantiation
 bst = BST()
 
-# assert bst.search_iterative(5) == True
-# assert bst.search_iterative(20) == False
-# print(bst.search_iterative(5))
-# print(bst.search_recursive(20))
-
-assert bst.level_order() == [7, 4, 9, 2, 5, 8, 12, 0, 16]
-print(bst.level_order())
-# assert bst.pre_order()   == [7, 4, 2, 0, 5, 9, 8, 12, 16]
-# assert bst.in_order()    == [0, 2, 4, 5, 7, 8, 9, 12, 16]
-# assert bst.post_order()  == [0, 2, 5, 4, 8, 16, 12, 9, 7]
 # write your assertions below
 # format
 # assert function_call == expected_output
